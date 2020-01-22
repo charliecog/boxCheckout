ff3536ead93314a45f519414af1210f98844f404
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 09:29:56 +0000

    basic setup for OrderDetailsEntity

---
+++ src/Classes/Entities/OrderDetailsEntity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BoxCheckout\Entities;
+
+use BoxCheckout\Interfaces\Sanitiseable;
+
+class OrderDetailsEntity extends ValidationEntity implements Sanitiseable
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function sanitiseData()
+    {
+        
+    }
+}
