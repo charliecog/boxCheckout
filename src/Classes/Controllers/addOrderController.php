2c7f91a9616fa70225a9fdd2b565ca47684e2bd5
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 11:50:19 +0000

    began pulling data out of request and instansiating entities

---
+++ src/Classes/Controllers/addOrderController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BoxCheckout\Controllers;
+
+use BoxCheckout\Models\OrderModel;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class addOrderController
+{
+    private $orderModel;
+
+    public function __construct(OrderModel $orderModel)
+    {
+        $this->orderModel = $orderModel;
+    }
+
+    /**
+     * Calls various methods to add the order to the db and send JSON back with the response
+     *
+     * @param Request $request HTTP request
+     * @param Response $response HTTP response
+     * @param array $args
+     * @return Response returns JSON with boxes data
+     */
+    public function __invoke(Request $request, Response $response, array $args) : Response
+    {
+        $data = [
+            'status' => false,
+            'message' => 'order not added to the db',
+            'data' => []
+        ];
+        $statusCode = 400;
+
+        $newOrderData = $request->getParsedBody();
+
+        $address = $this->orderModel->createAddressEntity(...$newOrderData['address']);
+
+
+
+        try {
+//            $insertedAddressId = $this->orderModel->getAllBoxes();
+
+
+
+
+
+
+        } catch (\PDOException $exception) {
+            $data['message'] = $exception->getMessage();
+        }
+
+        if (!empty($boxes)) {
+            $data = [
+                'status' => true,
+                'message' => 'Boxes retrieved',
+                'data' => $boxes
+            ];
+            $statusCode = 200;
+        }
+
+        return $response->withJson($data, $statusCode);
+    }
+}
