91ff8d857d5b54f3dd8c4061e52de8bd7642a65a
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 09:26:57 +0000

    adds getters and sanitisation to orderentity

--- src/Classes/Entities/OrderEntity.php
+++ src/Classes/Entities/OrderEntity.php
@@ -27,10 +27,83 @@ class OrderEntity extends ValidationEntity implements Sanitiseable
         $this->totalPrice = $totalPrice;
         $this->discountApplied = $discountApplied;
         $this->totalChargedPrice = $totalChargedPrice;
+
+        $this->sanitiseData();
     }
 
     public function sanitiseData()
     {
-        
+        self::validatePositiveInt($this->userId);
+        self::validatePositiveInt($this->deliveryId);
+        self::validatePositiveInt($this->paymentId);
+        self::validatePositiveFloat($this->totalPrice);
+        self::validatePositiveInt($this->discountApplied);
+        $this->validateDiscount();
+        self::validatePositiveFloat($this->totalChargedPrice);
+    }
+
+    /**
+     * Validates entity discount value, throws an Exception if not valid
+     *
+     * @return mixed, true if valid
+     * @throws \Exception if not a valid selection
+     */
+    public function validateDiscount() : bool
+    {
+        if( $this->discountApplied === 0 ||
+            $this->discountApplied === 15
+        ) {
+            return true;
+        } else {
+            throw new \Exception('discount is not valid');
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDeliveryId(): int
+    {
+        return $this->deliveryId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPaymentId(): int
+    {
+        return $this->paymentId;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotalPrice(): float
+    {
+        return $this->totalPrice;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDiscountApplied(): int
+    {
+        return $this->discountApplied;
+    }
+
+    /**
+     * @return float
+     */
+    public function getTotalChargedPrice(): float
+    {
+        return $this->totalChargedPrice;
     }
 }
