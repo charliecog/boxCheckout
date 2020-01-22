d94c22eeb732dfb14d696144df6c2248443348b7
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 08:45:33 +0000

    basic entity setup for an order

--- src/Classes/Entities/OrderEntity.php
+++ src/Classes/Entities/OrderEntity.php
@@ -2,7 +2,35 @@
 
 namespace BoxCheckout\Entities;
 
-class OrderEntity
-{
+use BoxCheckout\Interfaces\Sanitiseable;
 
+class OrderEntity extends ValidationEntity implements Sanitiseable
+{
+    protected $userId;
+    protected $deliveryId;
+    protected $paymentId;
+    protected $totalPrice;
+    protected $discountApplied;
+    protected $totalChargedPrice;
+
+    public function __construct(
+        int $userId,
+        int $deliveryId,
+        int $paymentId,
+        float $totalPrice,
+        int $discountApplied,
+        float $totalChargedPrice
+    ) {
+        $this->userId = $userId;
+        $this->deliveryId = $deliveryId;
+        $this->paymentId = $paymentId;
+        $this->totalPrice = $totalPrice;
+        $this->discountApplied = $discountApplied;
+        $this->totalChargedPrice = $totalChargedPrice;
+    }
+
+    public function sanitiseData()
+    {
+        
+    }
 }
