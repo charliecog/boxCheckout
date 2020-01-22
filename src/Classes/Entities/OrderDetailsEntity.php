5bd3d47cf16d435464fb03f0cd0e925278d83599
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 09:34:42 +0000

    OrderDetailsEntity contstructor, sanitiseData and getters

--- src/Classes/Entities/OrderDetailsEntity.php
+++ src/Classes/Entities/OrderDetailsEntity.php
@@ -6,13 +6,50 @@ use BoxCheckout\Interfaces\Sanitiseable;
 
 class OrderDetailsEntity extends ValidationEntity implements Sanitiseable
 {
-    public function __construct()
-    {
-        
+    protected $orderId;
+    protected $boxId;
+    protected $quantity;
+
+    public function __construct(
+        int $orderId,
+        int $boxId,
+        int $quantity
+    ) {
+        $this->orderId = $orderId;
+        $this->boxId = $boxId;
+        $this->quantity = $quantity;
+
+        $this->sanitiseData();
     }
 
     public function sanitiseData()
     {
-        
+        self::validatePositiveInt($this->orderId);
+        self::validatePositiveInt($this->boxId);
+        self::validatePositiveInt($this->quantity);
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrderId(): int
+    {
+        return $this->orderId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBoxId(): int
+    {
+        return $this->boxId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuantity(): int
+    {
+        return $this->quantity;
     }
 }
