b0bab64b4d15533af0f943023102c57613df0337
charliecog <charlie.coggans@mayden.co.uk>
2020-01-22 10:54:47 +0000

    adds addUser method

--- src/Classes/Models/OrderModel.php
+++ src/Classes/Models/OrderModel.php
@@ -3,6 +3,7 @@
 namespace BoxCheckout\Models;
 
 use BoxCheckout\Entities\AddressEntity;
+use BoxCheckout\Entities\UserEntity;
 
 class OrderModel
 {
@@ -31,6 +32,29 @@ class OrderModel
         $query->bindParam(':postcode', $address->getPostcode());
         $query->bindParam(':county', $address->getCounty());
         $query->bindParam(':country', $address->getCountry());
+        $query->execute();
+        return $this->db->lastInsertId();
+    }
+
+    /**
+     * Adds User data to the database.
+     *
+     * @param \BoxCheckout\Entities\UserEntity All the details of the user
+     *
+     * @return int The id of the inserted row.
+     */
+    public function addUser(UserEntity $user)
+    {
+        $query = $this->db->prepare(
+            "INSERT INTO `users` (`title`, `first_name`, `last_name`, `business_name`, `email`, `phone`, `secondary_phone`) VALUES (:title, :firstName, :lastName, :businessName, :email, :phone, :secondaryPhone);"
+        );
+        $query->bindParam(':title', $user->getTitle());
+        $query->bindParam(':firstName', $user->getFirstName());
+        $query->bindParam(':lastName', $user->getLastName());
+        $query->bindParam(':businessName', $user->getBusinessName());
+        $query->bindParam(':email', $user->getEmail());
+        $query->bindParam(':phone', $user->getPhone());
+        $query->bindParam(':secondaryPhone', $user->getSecondaryPhone());
         $query->execute();
         return $this->db->lastInsertId();
     }
